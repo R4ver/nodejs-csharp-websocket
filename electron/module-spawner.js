47eada45acc63cbcfc4afb6fafa25979943363ea
R4ver <raver13372@gmail.com>
2022-05-03 20:58:16 +0200

    Fixed ESLint issue causing it to not run. Added platform check for Linux when spawning .exe files

--- electron/module-spawner.js
+++ electron/module-spawner.js
@@ -1,41 +1,46 @@
 import { resolve } from "path";
 import { spawn } from "child_process";
 
-export default function SpawnModule(module, isExecuteable = false) {
+export default function SpawnModule( module, isExecuteable = false ) {
     let child;
+    const platform = process.platform;
 
     switch ( isExecuteable ) {
-        case true:
+    case true:
+        if ( platform === "linux" ) {
+            child = spawn( "msc", `../modules/release/${module}.exe` );
+        } else {
             child = spawn( resolve( __dirname, `../modules/release/${module}.exe` ) );
-            break;
+        }
+        break;
     
-        default:
-            child = spawn( resolve( __dirname, `../modules/release/${module}.js` ) );
-            break;
+    default:
+        child = spawn( resolve( __dirname, `../modules/release/${module}.js` ) );
+        break;
     }
 
-    child.stdout.setEncoding( 'utf8' );
-    child.stdout.on( 'data', function ( data ) {
+    child.stdout.setEncoding( "utf8" );
+    child.stdout.on( "data", function ( data ) {
         //Here is where the output goes
 
-        console.log( 'stdout: ' + data );
+        console.log( "stdout: " + data );
 
         data = data.toString();
     } );
 
-    child.stderr.setEncoding( 'utf8' );
-    child.stderr.on( 'data', function ( data ) {
+    child.stderr.setEncoding( "utf8" );
+    child.stderr.on( "data", function ( data ) {
         //Here is where the error output goes
 
-        console.log( 'stderr: ' + data );
+        console.log( "stderr: " + data );
 
         data = data.toString();
     } );
 
-    child.on( 'close', function ( code ) {
+    child.on( "close", function ( code ) {
         //Here you can get the exit code of the script
 
-        console.log( 'closing code: ' + code );
+        console.log( "closing code: " + code );
     } );
 
     return child;
